99f8dac306ad86c10e13e38a3b3816aa184d8156
sachinit254 <sachinit254@gmail.com>
2022-05-23 10:59:19 +0530

    UI development

---
+++ src/pages/hotel/Hotel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./hotel.css"
+const Hotel = () => {
+  return (
+    <div>Hotel</div>
+  )
+}
+
+export default Hotel
---
+++ src/pages/list/List.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./list.css";
+const List = () => {
+  return <div>List</div>;
+};
+
+export default List;
